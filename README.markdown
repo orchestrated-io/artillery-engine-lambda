bac82e33d80ad384a8790a459913078be9e01050
Warrick Zedi <warrick@orchestrated.io>
2017-09-25 11:17:01 +1000

    First commit

---
+++ README.markdown
@@ -0,0 +1,74 @@
+# Artillery.io AWS Lambda Plugin
+
+<p align="center">
+    <em>Load test AWS Lambda with <a href="https://artillery.io">Artillery.io</a></em>
+</p>
+
+Based on the [Kinesis Engine by Shoreditch](https://github.com/shoreditch-ops/artillery-engine-kinesis).
+
+## Usage
+
+**Important:** The plugin requires Artillery `1.5.8-3` or higher.
+
+### Install the plugin
+
+```
+# If Artillery is installed globally:
+npm install -g artillery-engine-lambda
+```
+
+### Use the plugin
+
+1. Set `config.target` to the name of the Lambda function
+2. Specify additional options in `config.lambda`:
+    - `region` - AWS region (**default**: `us-east-1`)
+3. Set the `engine` property of the scenario to `lambda`.
+4. Use `invoke` in your scenario to push data to the stream.
+5. Specify additional invocation parameters:
+    - `payload` - String or object with the payload to send to the Lambda function
+    - `invocationType` - Lambda invocation type. One of `Event`, `RequestResponse`, `DryRun`
+    - `logType` - One of `None`, `Tail`
+    - `qualifier` - Lambda qualifier
+    - `clientContext` - client context to pass to the Lambda function as context
+
+#### Example Script
+
+```yaml
+config:
+  target: "lambda_function_name"
+  lambda:
+    region: "us-east-1"
+  # Emulate 10 publishers
+  phases:
+    arrivalCount: 10
+    duration: 1
+  engines:
+    lambda: {}
+
+scenarios:
+  - name: "Invoke function"
+    flow:
+      - loop:
+        - invoke:
+           # data may be a string or an object. Objects
+           # will be JSON.stringified.
+           clientContext: "MyApp"
+           invocationType: "Event"
+           logType: "Tail"
+           payload: "Some payload"
+           qualifier: "1"
+        - think: 1
+        count: 100
+```
+
+(See [example.yml](example.yml) for a complete example.)
+
+### Run Your Script
+
+```
+AWS_PROFILE=dev artillery run my_script.yml
+```
+
+### License
+
+[MPL 2.0](https://www.mozilla.org/en-US/MPL/2.0/)
---
+++ index.js
@@ -0,0 +1,122 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+const Lambda = require('aws-sdk/clients/lambda');
+const debug = require('debug')('engine:lambda');
+const A = require('async');
+const _ = require('lodash');
+
+function LambdaEngine (script, ee, helpers) {
+  this.script = script;
+  this.ee = ee;
+  this.helpers = helpers;
+
+  return this;
+}
+
+LambdaEngine.prototype.createScenario = function createScenario (scenarioSpec, ee) {
+  const tasks = scenarioSpec.flow.map(rs => this.step(rs, ee));
+
+  return this.compile(tasks, scenarioSpec.flow, ee);
+};
+
+LambdaEngine.prototype.step = function step (rs, ee) {
+  const self = this;
+
+  if (rs.loop) {
+    const steps = rs.loop.map(loopStep => this.step(loopStep, ee));
+
+    return this.helpers.createLoopWithCount(rs.count || -1, steps);
+  }
+
+  if (rs.log) {
+    return function log (context, callback) {
+      return process.nextTick(function () { callback(null, context); });
+    };
+  }
+
+  if (rs.think) {
+    return this.helpers.createThink(rs, _.get(self.config, 'defaults.think', {}));
+  }
+
+  if (rs.function) {
+    return function (context, callback) {
+      let func = self.config.processor[rs.function];
+      if (!func) {
+        return process.nextTick(function () { callback(null, context); });
+      }
+
+      return func(context, ee, function () {
+        return callback(null, context);
+      });
+    };
+  }
+
+  if (rs.invoke) {
+    return function invoke (context, callback) {
+      const payload = typeof rs.invoke.payload === 'object'
+            ? JSON.stringify(rs.invoke.payload)
+            : String(rs.invoke.payload);
+
+      var params = {
+           ClientContext: rs.invoke.clientContext || "", //base64 encode
+           FunctionName: self.script.config.target, 
+           InvocationType: rs.invoke.invocationType || "Event", 
+           LogType: rs.invoke.logType || "Tail", 
+           Payload: payload,
+           Qualifier: rs.invoke.qualifier || "1"
+      };
+
+      ee.emit('request');
+      context.lambda.invoke(params, function (err, data) {
+        if (err) {
+          debug(err);
+          ee.emit('error', err);
+          return callback(err, context);
+        }
+
+        ee.emit('response', 0, 0, context._uid); // FIXME
+        debug(data);
+        return callback(null, context);
+      });
+    };
+  }
+
+  return function (context, callback) {
+    return callback(null, context);
+  };
+};
+
+LambdaEngine.prototype.compile = function compile (tasks, scenarioSpec, ee) {
+  const self = this;
+  return function scenario (initialContext, callback) {
+    const init = function init (next) {
+      let opts = {
+        region: self.script.config.lambda.region || 'us-east-1'
+      };
+
+      if (self.script.config.lambda.function) {
+        opts.endpoint = self.script.config.lambda.function;
+      }
+
+      initialContext.lambda = new Lambda(opts);
+      ee.emit('started');
+      return next(null, initialContext);
+    };
+
+    let steps = [init].concat(tasks);
+
+    A.waterfall(
+      steps,
+      function done (err, context) {
+        if (err) {
+          debug(err);
+        }
+
+        return callback(err, context);
+      });
+  };
+};
+
+module.exports = LambdaEngine;
