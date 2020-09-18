92723a643f8657af3ca20a38ac21ac82f2f9fd8a
Andres Vargas <andres@python3.ninja>
2020-09-18 12:24:29 -0500

    rollback eslint changes, yes i know it is too much changes

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-"use strict";
+'use strict';
 
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-const aws = require("aws-sdk");
-const debug = require("debug")("engine:lambda");
-const A = require("async");
-const _ = require("lodash");
-const helpers = require("artillery/core/lib/engine_util");
+const aws = require('aws-sdk');
+const debug = require('debug')('engine:lambda');
+const A = require('async');
+const _ = require('lodash');
+const helpers = require('artillery/core/lib/engine_util');
 
-const utils = require("./utils");
+const utils = require('./utils');
 
-function LambdaEngine(script, ee) {
+function LambdaEngine (script, ee) {
   this.script = script;
   this.ee = ee;
   this.helpers = helpers;
@@ -23,125 +23,111 @@ function LambdaEngine(script, ee) {
   return this;
 }
 
-LambdaEngine.prototype.createScenario = function createScenario(
-  scenarioSpec,
-  ee
-) {
+LambdaEngine.prototype.createScenario = function createScenario (scenarioSpec, ee) {
+
   // as for http engine we add before and after scenario hook
   // as normal functions in scenario's steps
-  const beforeScenarioFns = _.map(scenarioSpec.beforeScenario, function(
-    hookFunctionName
-  ) {
-    return { function: hookFunctionName };
-  });
-  const afterScenarioFns = _.map(scenarioSpec.afterScenario, function(
-    hookFunctionName
-  ) {
-    return { function: hookFunctionName };
-  });
+  const beforeScenarioFns = _.map(
+    scenarioSpec.beforeScenario,
+    function(hookFunctionName) {
+      return {'function': hookFunctionName};
+    });
+  const afterScenarioFns = _.map(
+    scenarioSpec.afterScenario,
+    function(hookFunctionName) {
+      return {'function': hookFunctionName};
+    });
 
   const newFlow = beforeScenarioFns.concat(
-    scenarioSpec.flow.concat(afterScenarioFns)
-  );
+    scenarioSpec.flow.concat(afterScenarioFns));
 
   scenarioSpec.flow = newFlow;
 
-  const tasks = scenarioSpec.flow.map(rs =>
-    this.step(rs, ee, {
-      beforeRequest: scenarioSpec.beforeRequest,
-      afterResponse: scenarioSpec.afterResponse
-    })
-  );
+  const tasks = scenarioSpec.flow.map(rs => this.step(rs, ee,  {
+    beforeRequest: scenarioSpec.beforeRequest,
+    afterResponse: scenarioSpec.afterResponse,
+  }));
 
   return this.compile(tasks, scenarioSpec.flow, ee);
 };
 
-LambdaEngine.prototype.step = function step(rs, ee, opts) {
+LambdaEngine.prototype.step = function step (rs, ee, opts) {
   opts = opts || {};
   let self = this;
 
   if (rs.loop) {
-    let steps = _.map(rs.loop, function(rs) {
+    let steps = _.map(rs.loop, function (rs) {
       return self.step(rs, ee, opts);
     });
 
-    return this.helpers.createLoopWithCount(rs.count || -1, steps, {
-      loopValue: rs.loopValue || "$loopCount",
-      overValues: rs.over,
-      whileTrue: self.config.processor
-        ? self.config.processor[rs.whileTrue]
-        : undefined
-    });
+    return this.helpers.createLoopWithCount(
+      rs.count || -1,
+      steps,
+      {
+        loopValue: rs.loopValue || '$loopCount',
+        overValues: rs.over,
+        whileTrue: self.config.processor
+          ? self.config.processor[rs.whileTrue] : undefined
+      });
   }
 
   if (rs.log) {
-    return function log(context, callback) {
-      return process.nextTick(function() {
-        callback(null, context);
-      });
+    return function log (context, callback) {
+      return process.nextTick(function () { callback(null, context); });
     };
   }
 
   if (rs.think) {
-    return this.helpers.createThink(
-      rs,
-      _.get(self.config, "defaults.think", {})
-    );
+    return this.helpers.createThink(rs, _.get(self.config, 'defaults.think', {}));
   }
 
   if (rs.function) {
-    return function(context, callback) {
+    return function (context, callback) {
       let func = self.config.processor[rs.function];
       if (!func) {
-        return process.nextTick(function() {
-          callback(null, context);
-        });
+        return process.nextTick(function () { callback(null, context); });
       }
 
-      return func(context, ee, function() {
+      return func(context, ee, function () {
         return callback(null, context);
       });
     };
   }
 
   if (rs.invoke) {
-    return function invoke(context, callback) {
+    return function invoke (context, callback) {
+
       context.funcs.$increment = self.$increment;
       context.funcs.$decrement = self.$decrement;
-      context.funcs.$contextUid = function() {
+      context.funcs.$contextUid = function () {
         return context._uid;
       };
-      const payload =
-        typeof rs.invoke.payload === "object"
-          ? JSON.stringify(rs.invoke.payload)
-          : String(rs.invoke.payload);
+
+      const payload = typeof rs.invoke.payload === 'object'
+        ? JSON.stringify(rs.invoke.payload)
+        : String(rs.invoke.payload);
+
       // see documentation for a description of these fields
       // https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/Lambda.html#invoke-property
       var awsParams = {
-        ClientContext: Buffer.from(rs.invoke.clientContext || "{}").toString(
-          "base64"
-        ),
+        ClientContext: Buffer.from(rs.invoke.clientContext || '{}').toString('base64'),
         FunctionName: rs.invoke.target || self.script.config.target,
-        InvocationType: rs.invoke.invocationType || "Event",
-        LogType: rs.invoke.logType || "Tail",
+        InvocationType: rs.invoke.invocationType || 'Event',
+        LogType: rs.invoke.logType || 'Tail',
         Payload: helpers.template(payload, context),
-        Qualifier: rs.invoke.qualifier || "$LATEST"
+        Qualifier: rs.invoke.qualifier || '$LATEST'
       };
+
       // build object to pass to hooks
       // we do not pass only aws params but also additional information
       // we need to make the engine work with other plugins
-      const params = _.assign(
-        {
-          url: context.lambda.endpoint.href,
-          awsParams: awsParams
-        },
-        rs.invoke
-      );
+      const params = _.assign({
+        url: context.lambda.endpoint.href,
+        awsParams: awsParams,
+      }, rs.invoke);
 
-      const beforeRequestFunctionNames = _.concat(
-        opts.beforeRequest || [],
-        rs.invoke.beforeRequest || []
-      );
+
+      const beforeRequestFunctionNames = _.concat(opts.beforeRequest || [], rs.invoke.beforeRequest || []);
 
       utils.processBeforeRequestFunctions(
         self.script,
@@ -155,7 +141,7 @@ LambdaEngine.prototype.step = function step(rs, ee, opts) {
             return callback(err, context);
           }
 
-          ee.emit("request");
+          ee.emit('request');
           const startedAt = process.hrtime();
 
           // after running "before request" functions
@@ -164,10 +150,11 @@ LambdaEngine.prototype.step = function step(rs, ee, opts) {
           awsParams.Payload = helpers.template(payload, context);
 
           // invoke lambda function
-          context.lambda.invoke(awsParams, function(err, data) {
+          context.lambda.invoke(awsParams, function (err, data) {
+
             if (err) {
               debug(err);
-              ee.emit("error", err);
+              ee.emit('error', err);
               return callback(err, context);
             } else if (data.FunctionError) {
               ee.emit("error", data.FunctionError);
@@ -210,20 +197,16 @@ LambdaEngine.prototype.step = function step(rs, ee, opts) {
             let delta = endedAt[0] * 1e9 + endedAt[1];
             ee.emit("response", delta, code, context._uid);
 
-            debug("response=", response);
-            helpers.captureOrMatch(params, response, context, function captured(
-              err,
-              result
-            ) {
-              if (result && result.captures) {
+            helpers.captureOrMatch(
+              params,
+              response,
+              context,
+              function captured(err, result) {
                 // TODO handle matches
-                let haveFailedCaptures = _.some(result.captures, function(
-                  v,
-                  k
-                ) {
-                  return v === "";
+                let haveFailedCaptures = _.some(result.captures, function(v, k) {
+                  return v === '';
                 });
-
+                
                 if (!haveFailedCaptures) {
                   _.each(result.captures, function(v, k) {
                     _.set(context.vars, k, v);
@@ -231,45 +214,44 @@ LambdaEngine.prototype.step = function step(rs, ee, opts) {
                 }
               }
 
-              const afterResponseFunctionNames = _.concat(
-                opts.afterResponse || [],
-                rs.invoke.afterResponse || []
-              );
+                const afterResponseFunctionNames = _.concat(opts.afterResponse || [], rs.invoke.afterResponse || []);
 
-              utils.processAfterResponseFunctions(
-                self.script,
-                afterResponseFunctionNames,
-                params,
-                response,
-                context,
-                ee,
-                function done(err) {
-                  if (err) {
-                    debug(err);
-                    return callback(err, context);
+                utils.processAfterResponseFunctions(
+                  self.script,
+                  afterResponseFunctionNames,
+                  params,
+                  response,
+                  context,
+                  ee,
+                  function done(err) {
+                    if (err) {
+                      debug(err);
+                      return callback(err, context);
+                    }
+    
+                    return callback(null, context);
                   }
-
-                  return callback(null, context);
-                }
-              );
-            });
+                );
+              }
+            );
+            
           });
         }
-      );
+      )
     };
   }
 
-  return function(context, callback) {
+  return function (context, callback) {
     return callback(null, context);
   };
 };
 
-LambdaEngine.prototype.compile = function compile(tasks, scenarioSpec, ee) {
+LambdaEngine.prototype.compile = function compile (tasks, scenarioSpec, ee) {
   const self = this;
-  return function scenario(initialContext, callback) {
-    const init = function init(next) {
+  return function scenario (initialContext, callback) {
+    const init = function init (next) {
       let opts = {
-        region: self.script.config.lambda.region || "us-east-1"
+        region: self.script.config.lambda.region || 'us-east-1'
       };
 
       if (self.script.config.lambda.function) {
@@ -277,29 +259,31 @@ LambdaEngine.prototype.compile = function compile(tasks, scenarioSpec, ee) {
       }
 
       initialContext.lambda = new aws.Lambda(opts);
-      ee.emit("started");
+      ee.emit('started');
       return next(null, initialContext);
     };
 
     let steps = [init].concat(tasks);
 
-    A.waterfall(steps, function done(err, context) {
-      if (err) {
-        debug(err);
-      }
+    A.waterfall(
+      steps,
+      function done (err, context) {
+        if (err) {
+          debug(err);
+        }
 
-      return callback(err, context);
-    });
+        return callback(err, context);
+      });
   };
 };
 
-LambdaEngine.prototype.$increment = function $increment(value) {
-  let result = Number.isInteger(value) ? (value += 1) : NaN;
+LambdaEngine.prototype.$increment = function $increment (value) {
+  let result = Number.isInteger(value) ? value += 1 : NaN;
   return result;
 };
 
-LambdaEngine.prototype.$decrement = function $decrement(value) {
-  let result = Number.isInteger(value) ? (value -= 1) : NaN;
+LambdaEngine.prototype.$decrement = function $decrement (value) {
+  let result = Number.isInteger(value) ? value -= 1 : NaN;
   return result;
 };
 
